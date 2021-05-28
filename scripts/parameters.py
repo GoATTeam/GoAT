52649a9f4520a1b14796755b393f2861dc3fc6c7
sm2686 <sm2686@cornell.edu>
2021-05-28 15:13:13 -0400

    Fix the bug in economic analysis

--- scripts/parameters.py
+++ scripts/parameters.py
@@ -1,15 +1,13 @@
 from mpmath import *
 mp.dps = 1000
 
-# cap vs num_chals (2^80; 330) (2^40; 280) (2^20; 250)
-
-num_challenges = mpf(280)
+num_challenges = mpf(170)
 grinding_cap = mpf(2**40)
 sec_par = mpf(80)
 
 code_rate = mpf(0.33)
 
-kappa_file_frac = mpf(0.7)
+downloadable_ratio = mpf(0.001)
 
 import math
 
@@ -25,7 +23,7 @@ print("Stored fraction due to grinding is only", stored_frac_2)
 
 print(stored_frac > stored_frac_2)
 
-unanswerable_frac = (code_rate * (1 + kappa_file_frac) + (1 - stored_frac)) ** num_challenges
+unanswerable_frac = (code_rate + downloadable_ratio  + (1 - stored_frac)) ** num_challenges
 
 print("Unanswerable fraction log base 2 is", math.log2(unanswerable_frac)) # This should be negligible in the security parameter
 
